e05ca3688c66c6dd13a7f80e4d4407a9af384734
Imam-Hossain <zunayedemon6t9@gmail.com>
2022-09-29 21:00:57 +0600

    collapse file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Collapse from './components/Collapse/Collapse';
 import Home from './components/home/Home';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div>
 
      <Home></Home>
+     <Collapse></Collapse>
   
       
      
